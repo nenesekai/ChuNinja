50e948e7a35c0469116bc2a7d86e11594fd38a79
Asiimoviet <asiimoviet@gmail.com>
2024-10-19 02:02:43 -0700

    Initial Commit

---
+++ ChuNinja/ChuNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuNinjaApp.swift
+//  ChuNinja
+//
+//  Created by Kusanagi Nene on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ChuNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
